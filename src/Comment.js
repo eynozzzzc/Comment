b963108d41a16383e3dbb93136e8fd6e30eec300
eynozzzzc <eynocattfos@csu.edu.cn>
2022-05-14 01:25:02 +0800

    second commit

---
+++ src/Comment.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Comment extends Component {
+    render() {
+        return (
+            <div className="comment">
+                <div className="comment-user">
+                    <span>{this.props.comment.username}</span>
+                </div>
+                <p>{this.props.comment.content}</p>
+            </div>
+        );
+    }
+}
+export default Comment;
---
+++ src/CommentInput.js
@@ -0,0 +1,78 @@
+import { Component } from "react";
+
+class CommentInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            content: ''
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleUsernameChange(event) {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    handleContentChange(event) {
+        this.setState({
+            content: event.target.value
+        });
+    }
+
+    handleSubmit() {
+        if (this.props.onSubmit) {
+            const username = this.state.username;
+            const content = this.state.content;
+            this.props.onSubmit({
+                username,
+                content
+            });
+        }
+        this.setState({
+            content: ''
+        });
+    }
+
+    render() {
+        return (
+            <div className="comment-input">
+                <div className="comment-field">
+                    <span className="comment-field-name"> 用户名：</span>
+                    <div className="comment-field-input">
+                        <input
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}
+                        >
+                        </input>
+                    </div>
+                </div>
+
+                <div className="comment-field">
+                    <span className="comment-field-name"> 评论内容：</span>
+                    <div className="comment-field-input">
+                        <textarea
+                            value={this.state.content}
+                            onChange={this.handleContentChange}
+                        >
+                        </textarea>
+                    </div>
+                </div>
+
+                <div className="comment-field-button">
+                    <button
+                        onClick={this.handleSubmit}>
+                        发布
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CommentInput;
